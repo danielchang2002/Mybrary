b4f7f3e4f0a9290b63916bbdf3e72269b5020efd
danielchang2002 <danielchang2002@gmail.com>
2021-07-28 15:08:23 -0500

    intitial author routes

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
@@ -14,9 +16,9 @@ app.use(expressLayouts);
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-console.log("database url: " + process.env.DATABASE_URL);
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,5 +28,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
