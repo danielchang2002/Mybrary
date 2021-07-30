ef3f5d726f4946a7a6ae9ca0603249b05928cd16
danielchang2002 <danielchang2002@gmail.com>
2021-07-30 13:35:34 -0500

    finished author routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -13,6 +14,7 @@ const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
